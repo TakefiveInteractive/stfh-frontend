a016b6051d14e19fcfc19e4257a2cbcc5e48f83b
plantvsbirds <plantvsbird@gmail.com>
2017-02-25 20:32:03 -0600

    init room layout

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    <h1 v-if="$route.params.id == 'undefined'">Go get a room</h1>
+  <div class="col-md-offset-7 col-md-4 get-a-room"
+       v-if="$route.params.id == 'undefined'">
+    <h1>
+      Go get a room
+      &nbsp;
+      <i class="glyphicon glyphicon-menu-up"></i>
+    </h1>
   </div>
+    <div class="col-sm-12 left-big" v-else>
+      <section class="big">
+        
+      </section>
+      <section>
+        
+      </section>
+      <section>
+        
+      </section>
+    </div>
 </template>
 
 <script>
@@ -13,8 +29,46 @@ export default {
 }
 </script>
 
-<style scoped>
-h1 {
-  text-align: center;
+<style lang="scss" scoped>
+.get-a-room {
+  h1 {
+    text-align: right;
+  }
+  .glyphicon {
+    animation: upw 2s ease-out infinite;
+  }
+  @keyframes upw {
+      from {
+        transform: translateY(30px);
+        opacity: .3;
+      }
+      to {
+        transform: translateY(-10px);
+      }
+  }
+}
+
+div.left-big {
+  section {
+    width: 47.5%;
+    margin-left: 5%;
+    margin-bottom: 5%;
+    height: 42.5vh;
+
+    background: #111;
+    border-radius: 10px;
+
+    float: left;
+
+    &.big {
+      margin-left: 0;
+
+      clear: left;
+      height: 90vh;
+      background: #222;
+    }
+  }
+
+  margin-bottom: 10vh;
 }
 </style>
