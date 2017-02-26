1e000ada7b521699cb56607b6f86026ffd05d36d
plantvsbirds <plantvsbird@gmail.com>
2017-02-25 23:16:18 -0600

    use vue-monaco-editor

---
+++ src/components/Editor.vue
@@ -0,0 +1,31 @@
+<template>
+  <MonacoEditor
+    :height="editorHeight()"
+    :language="editorLang"
+    :code="editorCode"
+    :options="{readOnly : true, fontSize : 18}"
+    @mounted="editorMounted"
+    @codeChange="() => {}"
+    >
+  </MonacoEditor>
+</template>
+<script>
+import MonacoEditor from 'vue-monaco-editor'
+
+export default {
+  name: 'room',
+  props: ['editorLang', 'editorCode', 'editorMounted'],
+  data () {
+    return { }
+  },
+  components: {
+    MonacoEditor
+  },
+  methods: {
+    editorHeight: function() {
+      return window.outerHeight * 0.9;
+      // 90vh
+    }
+  }
+}
+</script>
